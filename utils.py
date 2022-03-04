9e90a2c9e97b328571f3724b0897baa4ed7a65d9
Daniel Barzilai <dbarzilai7@cs.huji.ac.il>
2022-03-04 22:01:31 +0200

    minor_fix

--- utils.py
+++ utils.py
@@ -5,17 +5,20 @@ import torch
 import yaml
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-CONF_PATH = sys.argv[1]  # "./conf.yaml"
 IMAGENET_NORMALIZATION_VALS = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
 
-def load_conf():
-    with open(CONF_PATH, "r") as f:
+
+def load_conf(path):
+    with open(path, "r") as f:
         cfg = yaml.safe_load(f)
-    seed = cfg['seed']
+    return cfg
+
+
+def set_seed(seed):
+    seed = seed
     if seed == -1:
         seed = np.random.randint(2 ** 32 - 1, dtype=np.int64)
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     print(f'running with seed: {seed}.')
-    return cfg
