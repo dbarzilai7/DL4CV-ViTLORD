874ee395c4533b7bf74c0fae865b0ba195c1ddca
Netanel Tamir <tamir.netanel@gmail.com>
2022-03-05 21:14:50 +0200

    Fixed ViT loss 2

--- losses.py
+++ losses.py
@@ -62,9 +62,9 @@ class LossG(torch.nn.Module):
         for a, b in zip(inputs, outputs):  # avoid memory limitations
             a = self.global_transform(a)
             b = self.global_transform(b)
-            target_keys_self_sim = self.extractor.get_keys_self_sim_from_input(a.unsqueeze(0), layer_num=11)
+            keys_ssim = self.extractor.get_keys_self_sim_from_input(b.unsqueeze(0), layer_num=11)
             with torch.no_grad():
-                keys_ssim = self.extractor.get_keys_self_sim_from_input(b.unsqueeze(0), layer_num=11)
+                target_keys_self_sim = self.extractor.get_keys_self_sim_from_input(a.unsqueeze(0), layer_num=11)
             loss += F.mse_loss(keys_ssim, target_keys_self_sim)
         return loss/len(outputs)
 
@@ -73,9 +73,9 @@ class LossG(torch.nn.Module):
         for a, b in zip(outputs, inputs):  # avoid memory limitations
             a = self.global_transform(a).unsqueeze(0).to(DEVICE)
             b = self.global_transform(b).unsqueeze(0).to(DEVICE)
+            cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
             with torch.no_grad():
-                cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
-            target_cls_token = self.extractor.get_feature_from_input(b)[-1][0, 0, :]
+                target_cls_token = self.extractor.get_feature_from_input(b)[-1][0, 0, :]
             loss += F.mse_loss(cls_token, target_cls_token)
         return loss/len(outputs)
 
@@ -84,9 +84,9 @@ class LossG(torch.nn.Module):
         for a, b in zip(inputs, outputs):
             a = self.global_transform(a)
             b = self.global_transform(b)
-            keys_a = self.extractor.get_keys_from_input(a.unsqueeze(0), 11)
             with torch.no_grad():
-                keys_b = self.extractor.get_keys_from_input(b.unsqueeze(0), 11)
+                keys_a = self.extractor.get_keys_from_input(a.unsqueeze(0), 11)
+            keys_b = self.extractor.get_keys_from_input(b.unsqueeze(0), 11)
             loss += F.mse_loss(keys_a, keys_b)
         return loss/len(outputs)
 
